311c1e47b45c9aedf39619cf6b1508ddb67db1c0
Valerie Ellis <valerie.ellis@flatironschool.com>
2024-02-24 20:43:08 -0500

    submitting that assignment that took 2 days and a pro dev

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {
+    streetAddress: "11 Broadway",
+    name: "Sam",
+  };
+  
+
+  function updateEmployeeWithKeyAndValue(obj,key,value){
+    const copy = {...obj};
+    copy[key] = value;
+    return copy;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+  obj[key] = value;
+  
+  return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key, value){
+  const employeecopy = {...obj}
+  delete employeecopy [key];
+
+  return employeecopy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value){
+  delete obj [key];
+  return employee;
+}
